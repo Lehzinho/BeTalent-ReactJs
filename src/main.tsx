0555dd290f3f7b7dc314a059bf847a6c0823354f
Alexandre Toulios <99737712+Lehzinho@users.noreply.github.com>
2025-07-07 22:24:22 -0300

    feat: creates `home` and adds all the styles configurations as well reremoving old styles files

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { router } from "./App";
+import { RouterProvider } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import { DefaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <ThemeProvider theme={DefaultTheme}>
+      <GlobalStyle />
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  </React.StrictMode>
+);
