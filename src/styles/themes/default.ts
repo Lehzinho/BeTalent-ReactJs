4cb5b6d25ae9236c06f07bb302e5d384136dfc29
Alexandre Toulios <99737712+Lehzinho@users.noreply.github.com>
2025-07-07 23:17:54 -0300

    refactor: adds `themes` and configure them in the `App` with routes and `layouts`

--- src/styles/themes/default.ts
+++ src/styles/themes/default.ts
@@ -1,1 +1,17 @@
-export const DefaultTheme = {};
+export const DefaultTheme = {
+  COLORS: {
+    "Blue/Primary": "#0500FF",
+    "Blue 10": "#EDEFFB",
+    "Black/Neutral": "#1C1C1C",
+    "Gray_20/Neutral": "#9E9E9E",
+    "Gray_10/Neutral": "#DFDFDF",
+    "Gray_05/Neutral": "#F5F5F5",
+    "Gray_00/Neutral": "#F0F0F0",
+    "White/Neutral": "#FFFFFF",
+  },
+  FONTS: {
+    H1: 'normal 500 20px "Helvetica Neue", sans-serif',
+    H2: 'normal 500 16px "Helvetica Neue", sans-serif',
+    H3: 'normal 400 16px "Helvetica Neue", sans-serif',
+  },
+} as const;
